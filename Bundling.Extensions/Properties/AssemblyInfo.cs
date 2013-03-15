975fa0d381e513bfebe3e3d39647b910ee906339
unger <magnus_unger@hotmail.com>
2013-03-15 21:29:25 +0100

    -created nuget package -downgraded to net4

--- Bundling.Extensions/Properties/AssemblyInfo.cs
+++ Bundling.Extensions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bundling.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extensions for Microsoft ASP.NET Web Optimization Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Magnus Unger")]
 [assembly: AssemblyProduct("Bundling.Extensions")]
 [assembly: AssemblyCopyright("Copyright © Magnus Unger 2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
