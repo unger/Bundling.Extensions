48e7c102706440cc4c5b6fd84f209277773da920
unger <magnus_unger@hotmail.com>
2013-03-16 01:03:04 +0100

    Added BundlingHelper

---
+++ Bundling.Extensions/BundlingHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Bundling.Extensions
+{
+	using System.Web;
+
+	using Bundling.Extensions.Renderers;
+
+	public static class BundlingHelper
+	{
+		public static IHtmlString RenderScript(string path)
+		{
+			return new ScriptRenderer().Render(path);
+		}
+
+		public static IHtmlString RenderStyles(string path)
+		{
+			return new StylesRenderer().Render(path);
+		}
+	}
+}
