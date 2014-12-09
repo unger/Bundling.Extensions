53ab37cbfc47726589c28e590a0c0bc15b075d75
unger <magnus_unger@hotmail.com>
2014-12-09 22:26:45 +0100

    added MyGet.bat

---
+++ MyGet.bat
@@ -0,0 +1,25 @@
+@echo off
+
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=.nuget\nuget.exe
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Bundling.Extensions\Bundling.Extensions.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+
+%nuget% pack "Bundling.Extensions\Bundling.Extensions.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
